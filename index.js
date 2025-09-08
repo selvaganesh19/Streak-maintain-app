2a711195d75dd0a20a4ac247b05eafba0d1268ab
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-09-08 11:49:52 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -4,20 +4,41 @@ import simpleGit from "simple-git";
 import random from "random";
 
 const path = "./data.json";
-const date = moment().subtract(5, 'days').format();
 
-const makeCommits = (n) => {
+const makeCommits = (n, targetDate) => {
     if(n === 0) return simpleGit().push();
-    const x = random.int(0, 54);
-    const y = random.int(0, 6);
-    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+    
+    // Use the target date or generate a random date within the past year
+    let commitDate;
+    if (targetDate) {
+        commitDate = moment(targetDate).format();
+    } else {
+        const x = random.int(0, 54);
+        const y = random.int(0, 6);
+        commitDate = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+    }
+    
     const data = {
-        date: date,
+        date: commitDate,
     };
-    console.log(date);
+    
+    console.log(`Making commit for: ${commitDate}`);
+    
     jsonfile.writeFile(path, data, () => {
-        simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this,--n));
+        simpleGit().add([path]).commit(`Commit for ${commitDate}`, { "--date": commitDate }, makeCommits.bind(this, --n, targetDate));
     });
 };
 
-makeCommits(1);
+// Usage examples:
+// For a specific date:
+makeCommits(1, "2025-06-25"); // Specific date
+
+// For multiple commits on the same date:
+// makeCommits(5, "2024-12-25");
+
+// For random dates (original behavior):
+// makeCommits(1);
+
+// For a range of dates, you can call it multiple times:
+// const dates = ["2024-12-24", "2024-12-25", "2024-12-26"];
+// dates.forEach(date => makeCommits(1, date));
